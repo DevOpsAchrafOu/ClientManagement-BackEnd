b0767de142ff6163830a58a2eb8b2b0278c76820
achrafOuakka <ouakka.achraf@gmail.com>
2023-03-27 17:12:38 +0000

    more update ifor regler problem in mode 'prod'

--- src/main/java/ma/mang/be/api/SmartClientManagementBackEndApplication.java
+++ src/main/java/ma/mang/be/api/SmartClientManagementBackEndApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.integration.IntegrationAutoConfiguration;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -17,6 +18,8 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 
 @SpringBootApplication
+@EnableAutoConfiguration(
+exclude = {DataSourceAutoConfiguration.class, HibernateJpaAutoConfiguration.class})
 @EnableSwagger2
 public class SmartClientManagementBackEndApplication {
 
